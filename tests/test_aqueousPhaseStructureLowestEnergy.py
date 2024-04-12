bc470a35299f6844b806dee23b30d88306345a05
Lareine Jeng <lareine@google.com>
2024-04-12 16:59:34 -0400

    Write first unit test
    Write aqueousPhaseStructureLowestEnergy test for ethane

--- tests/test_aqueousPhaseStructureLowestEnergy.py
+++
@@ -1,9 +0,0 @@
-from src.methods import aqueousPhaseStructureLowestEnergy
-
-def test_aqueousPhaseStructureLowestEnergy(test_smiles_list, atom_count_list):
-    for test_smiles, atom_count in zip(test_smiles_list, atom_count_list):
-        dimensions = 3
-        elements, pos = aqueousPhaseStructureLowestEnergy(test_smiles)
-        assert len(elements) == atom_count
-        assert pos.shape[0] == atom_count
-        assert pos.shape[1] == dimensions
