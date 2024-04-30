343b6ca1e11dda7646f930bb3b5a5b54b73308ec
Andrew Kim <andrew.taehon.kim@gmail.com>
2024-04-29 20:28:09 -0400

    added comments and corrected minor bugs

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from src.methods import *
+from src.molecule import Molecule, dumpReferenceStates
 CO2 = 'C(=O)=O'
 H2O = 'O'
 BENZENE = 'C1=CC=CC=C1'
 ETHANE = 'CC'
 
+dumpReferenceStates()
 
-H2O =smiles_to_properties(H2O)
+water = Molecule(H2O)
+# H2O =smiles_to_properties(H2O)
 # ETHANE =smiles_to_properties(ETHANE)
 # CO2 =smiles_to_properties(CO2)
 # BENZENE = smiles_to_properties(BENZENE)
